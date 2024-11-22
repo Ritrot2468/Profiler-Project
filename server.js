6b806ac635bb2f37f6571069ae067bc5a39e42a9
Ritrot2468 <108647374+Ritrot2468@users.noreply.github.com>
2024-11-22 23:35:56 +0000

    mdoified cors

--- server.js
+++ server.js
@@ -12,29 +12,25 @@ dotenv.config();
 
 const app = express();
 const BACKEND_URL = process.env.BACKEND_URL;
-const allowedOrigins = [
-  "http://127.0.0.1:3000", 
-  "http://localhost:3000", 
-  BACKEND_URL
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+// const allowedOrigins = [
+//   "http://127.0.0.1:3000", 
+//   "http://localhost:3000", 
+//   BACKEND_URL
+// ];
 
 // app.use(cors({
-//   origin: BACKEND_URL, 
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
 //   methods: ["GET", "POST", "PUT", "DELETE"],
 //   allowedHeaders: ["Content-Type", "Authorization"],
-// }))
+// }));
+
+app.use(cors())
 const PORT = process.env.PORT || 5000;
 
 const API_KEY = process.env.GOOGLE_API_KEY;
