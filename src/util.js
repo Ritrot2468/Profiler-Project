2d091c6d8724202d4d6f82748a3f26c48c00103d
Ritrot2468 <108647374+Ritrot2468@users.noreply.github.com>
2025-04-04 07:55:18 +0000

    modified util.js to fix instantiation of googleSearch service class

--- src/util.js
+++ src/util.js
@@ -4,15 +4,14 @@ const ScoreCalculator = require("../helpers/ScoreCalculator");
 const AccountBuilder = require("../helpers/AccountBuilder");
 const { sendDataToOpenAI } = require("../services/OpenAIService");
 const config = require("../config");
-const API_KEY = process.env.GOOGLE_API_KEY;
-const SEARCH_ENGINE_ID = process.env.SEARCH_ENGINE_ID;
+//const API_KEY = process.env.GOOGLE_API_KEY;
+//const SEARCH_ENGINE_ID = process.env.SEARCH_ENGINE_ID;
 
-
-
+const googleSearch = new GoogleSearchService(config.API_KEY, config.SEARCH_ENGINE_ID);
 
 async function parseAndSaveFundingAmounts(accountName, productCode, segment) {
-    const GoogleSearchService = GoogleSearchService(API_KEY, SEARCH_ENGINE_ID);
-     const googleResults = await GoogleSearchService.search(accountName);
+    //const GoogleSearchServiceClass = GoogleSearchService(API_KEY, SEARCH_ENGINE_ID);
+    const googleResults = await googleSearch.search(accountName);
     const gptResponse = await sendDataToOpenAI(accountName);
     const fundingResults = FundingParser.extractAll(googleResults);
 
